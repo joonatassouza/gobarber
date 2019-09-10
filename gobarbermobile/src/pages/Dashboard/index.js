8e8f77c3bd890521d1b1ec9a0228151b9f742976
Jonatas Souza <jonatas@easycomtec.com>
2019-09-10 09:11:01 -0300

    Commit all

---
+++ gobarbermobile/src/pages/Dashboard/index.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { withNavigationFocus } from 'react-navigation';
+
+import api from '~/services/api';
+
+import Background from '~/components/Background';
+import Appointment from '~/components/Appointment';
+
+import { Container, Title, List } from './styles';
+
+function Dashboard({ isFocused }) {
+  const [appointments, setAppointments] = useState([]);
+
+  async function loadAppointments() {
+    const response = await api.get('appointments');
+
+    setAppointments(response.data);
+  }
+
+  useEffect(() => {
+    if (isFocused) {
+      loadAppointments();
+    }
+  }, [isFocused]);
+
+  async function handleCancel(id) {
+    const response = await api.delete(`appointments/${id}`);
+
+    setAppointments(
+      appointments.map(appointment =>
+        appointment.id === id
+          ? { ...appointment, canceled_at: response.data.canceled_at }
+          : appointment
+      )
+    );
+
+    const appointmentCanceledIndex = appointments.findIndex(
+      app => app.id === id
+    );
+    if (appointmentCanceledIndex >= 0) {
+      const validAppointments = appointments;
+
+      validAppointments.splice(appointmentCanceledIndex, 1);
+
+      setAppointments(validAppointments);
+    }
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Title>Agendamentos</Title>
+
+        <List
+          data={appointments}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => (
+            <Appointment onCancel={() => handleCancel(item.id)} data={item} />
+          )}
+        />
+      </Container>
+    </Background>
+  );
+}
+
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Agendamentos',
+  // eslint-disable-next-line react/prop-types
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="event" size={20} color={tintColor} />
+  ),
+};
+
+export default withNavigationFocus(Dashboard);
