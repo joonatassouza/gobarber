bd6f82e33b09b9e99bba6d70cace4a1a105b81b1
Jonatas Souza <jonatas@easycomtec.com>
2019-09-10 09:19:33 -0300

    Add web

---
+++ web/src/pages/SignUp/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import logo from '~/assets/logo.svg';
+
+import { signUpRequest } from '~/store/modules/auth/actions';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+  confirmPassword: Yup.string()
+    .required()
+    .when('password', (password, field) =>
+      password ? field.required().oneOf([Yup.ref('password')]) : field
+    ),
+});
+
+export default function SignUp() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password, confirmPassword }) {
+    dispatch(signUpRequest(name, email, password, confirmPassword));
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirme sua senha"
+        />
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
+}
